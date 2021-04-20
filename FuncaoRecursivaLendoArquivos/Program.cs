afd7e0847ea63b4c482b86c7d320631d9d88c9bd
Lucas <lucasgunner3@gmail.com>
2021-04-20 19:37:36 -0300

    Refatorando código para que seja possível ler vários arquivos

--- FuncaoRecursivaLendoArquivos/Program.cs
+++ FuncaoRecursivaLendoArquivos/Program.cs
@@ -7,21 +7,31 @@ namespace FuncaoRecursivaLendoArquivos
     class Program
     {
         //Programa que vai ler os arquivos de uma pasta chamada ArquivosDotNetRead
-        private static void LerArquivo(string nome_arquivo)
+        private static void LerArquivo(string nome,int numero_arquivo)
         {
-            using (StreamReader arquivo = File.OpenText(nome_arquivo)) // Linha que abre o arquivo
+            string diretorio = @"C:\ArquivosDotNetRead\";
+            string nome_arquivo = $"{diretorio}{nome}{numero_arquivo}.txt";
+            if (File.Exists(nome_arquivo))
             {
-                string linha;
-                while ((linha = arquivo.ReadLine()) != null) // Código que faz a leitura das linhas
+                using (StreamReader arquivo = File.OpenText(nome_arquivo)) // Linha que abre o arquivo
                 {
-                    Console.WriteLine(linha); 
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null) // Código que faz a leitura das linhas
+                    {
+                        Console.WriteLine(linha);
+                    }
                 }
+            } else
+            {
+                Console.ReadKey();
             }
+
+            LerArquivo(nome,numero_arquivo+1);
         }
 
         static void Main(string[] args)
         {
-            LerArquivo(@"C:\ArquivosDotNetRead\arq1.txt");
+            LerArquivo("arq", 1);
         }
     }
 }
