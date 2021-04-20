b7093fa54cbf698dbca6c2824b57fba8c2f1b09f
Lucas <lucasgunner3@gmail.com>
2021-04-20 19:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ FuncaoRecursivaLendoArquivos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace FuncaoRecursivaLendoArquivos
+{
+    class Program
+    {
+        //Programa que vai ler os arquivos de uma pasta chamada ArquivosDotNetRead
+        private static void LerArquivo(string arq)
+        {
+            using (StreamReader arquivo = File.OpenText(arq))
+            {
+                string linha;
+                while ((linha = arquivo.ReadLine()) != null)
+                {
+                    ThreadPool.QueueUserWorkItem(ProcessaLinha, linha);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
